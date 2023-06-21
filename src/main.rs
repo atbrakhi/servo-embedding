d2e2fe3b092073fd5f326ef4bc257cc410d75c3b
atbrakhi <atbrakhi@gmail.com>
2023-06-21 16:21:35 +0200

    remove comment

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate simpleservo;
 
-// use simpleservo::servo_version;
 fn main() {
     println!("Servo version: {}", simpleservo::servo_version());
+    println!("Is url valid: {}", simpleservo::is_uri_valid("https://servo.org/"));
 }
